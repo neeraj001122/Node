94abb369389345fbb4741ca72d9940229477c357
neeraj001122 <admobid0011>
2022-12-31 19:15:24 +0530

    dynamic response

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
 const http = require('http');
 const server = http.createServer((req,res) =>{
-   console.log('Neeraj')
+   res.setHeader('Content-Type','text/html')
+   if(req.url === '/home')
+   {
+    res.write('<h1>Welcome home</h1>')
+   }
+   else if(req.url === '/about')
+   {
+      res.write('<h1>Welcome to about us page</h1>')
+   }
+   else if(req.url === '/node')
+   {
+      res.write('<h1>Welcome to my Node project</h1>')
+   }
+   res.end()
 })
 
-server.listen(4000);
+server.listen(3000);
 
