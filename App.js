44d86aa2c18a85642de4cf35c792bdfb1bf7f67e
neeraj001122 <admobid0011>
2023-01-01 15:56:42 +0530

    Form

--- App.js
+++ App.js
@@ -1,19 +1,35 @@
 const http = require('http');
+const fs = require('fs');
 const server = http.createServer((req,res) =>{
    res.setHeader('Content-Type','text/html')
-   if(req.url === '/home')
+   if(req.url === '/')
    {
-    res.write('<h1>Welcome home</h1>')
+      fs.readFile('message.txt', {encoding: 'utf-8'} ,(err,data) => {
+         if(err){
+            console.log(err)
+         }
+      console.log(data)
+    res.write(`<p>${data}<p>`)
+
+    res.write('<form action="/message"method="POST"><input type="text" name="message" /><button type="submit">Send</button></form>') 
+    return res.end()
+   })
    }
-   else if(req.url === '/about')
-   {
-      res.write('<h1>Welcome to about us page</h1>')
+   if(req.url === '/message' && req.method === 'POST'){
+      const body = [];
+      req.on('data', (chunk) => {
+         body.push(chunk)
+      })
+      return req.on('end', () => {
+         const parsedBody = Buffer.concat(body).toString();
+         const message = parsedBody.split('=')[1];
+         fs.writeFileSync('message.txt', message);
+         res.statusCode=302;
+         res.setHeader('Location', '/')
+         return res.end()
+      }) 
    }
-   else if(req.url === '/node')
-   {
-      res.write('<h1>Welcome to my Node project</h1>')
-   }
-   res.end()
+   res.end()   
 })
 
 server.listen(3000);
