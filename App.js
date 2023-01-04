e59c81cfe95bbace63e59ec0bb427c6edf20bdba
neeraj001122 <admobid0011>
2023-01-04 06:44:17 +0530

    Product res

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 const http = require('http');
+const bodyParser = require('body-parser')
 
 const express = require('express')
 
 const app = express();
 
-app.use((req,res,next) => {
-    next();
-})
+app.use(bodyParser.urlencoded({extended:false}))
 
-app.use((req,res,next) => {
-    res.send({ key1: 1 })
+app.use('/add-product',(req,res,next) => {
+    res.send('<form action = "/" method="POST"><input type="text" name="title"/><input type="Number" name="Number"/><button>Send</button></form>')
+    res.redirect('/')
+})  
+
+app.use('/',(req,res,next) => {
+    res.send('<h1>Welcome to the application</h1>')
+    console.log(req.body)
 })
 
 app.listen(3000)    
